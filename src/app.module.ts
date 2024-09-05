7684511114cff50d406c3a2122fa4a963d511be7
kateyohannes <kateyohannes@gmail.com>
2024-09-05 18:02:40 +0300

    initialize tracker controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TrackerController } from './tracker/tracker.controller';
+import { TrackerService } from './tracker/tracker.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, TrackerController],
+  providers: [AppService, TrackerService],
 })
 export class AppModule {}
