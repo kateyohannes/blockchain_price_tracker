241d7c585a2db0b7e133f3c50a70f068a5fbdccc
kateyohannes <kateyohannes@gmail.com>
2024-09-07 12:45:43 +0300

    add database module and config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    HttpModule,
+    DatabaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
