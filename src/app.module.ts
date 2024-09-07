7f90bf0d2afce87f1440b66fbc89036842ebcabf
kateyohannes <kateyohannes@gmail.com>
2024-09-07 13:32:32 +0300

    add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { TrackerModule } from './tracker/tracker.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     HttpModule,
     DatabaseModule,
+    TrackerModule,
   ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
