f662a6409a8b8522ab30fa937005aee4b48d6515
kateyohannes <kateyohannes@gmail.com>
2024-09-07 11:49:50 +0300

    remove tracker controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TrackerController } from './tracker/tracker.controller';
-import { TrackerService } from './tracker/tracker.service';
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [HttpModule],
-  controllers: [AppController, TrackerController],
-  providers: [AppService, TrackerService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
