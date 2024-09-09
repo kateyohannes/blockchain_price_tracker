2b7e0f399dccb10cc4cd3cb8ff0b7a05dd10f65b
kateyohannes <kateyohannes@gmail.com>
2024-09-09 11:59:26 +0300

    simple modification

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,6 @@ import { TrackerModule } from './tracker/tracker.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { MailService } from './mail/mail.service';
 import { MailController } from './mail/mail.controller';
-// import { MoralisModule } from 'nestjs-moralis';
 
 @Module({
   imports: [
@@ -15,10 +14,6 @@ import { MailController } from './mail/mail.controller';
     HttpModule,
     DatabaseModule,
     TrackerModule,
-    // MoralisModule.register({
-    //   apiKey:
-    //     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJub25jZSI6IjBmNjkxZjEyLWFjYzYtNGNiMy1hMGY4LWY2Mjk1ZmJjMGUyMCIsIm9yZ0lkIjoiNDA3MjE4IiwidXNlcklkIjoiNDE4NDM3IiwidHlwZUlkIjoiNDYwZGIwNTMtYWY4MC00YjdlLTgzNGUtMjJlMjhmNTUzOWQ0IiwidHlwZSI6IlBST0pFQ1QiLCJpYXQiOjE3MjU1NDYyNTcsImV4cCI6NDg4MTMwNjI1N30.Nr07HrVULkpyv1UVCGt2GmJjkx2lQugiZw-oFmV6IRI',
-    // }),
   ],
   controllers: [MailController],
   providers: [MailService],
